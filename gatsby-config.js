4dd063d4fdd92f0629a658b93a754534c9affdc5
Chase Turner <chasegregoryturner@gmail.com>
2019-12-02 03:43:29 -0500

    Banshee Babe Web Content Initial

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+    plugins: [
+        "gatsby-plugin-theme-ui",
+        "gatsby-plugin-mdx",{
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Banshee Babe Boutique`,
+                short_name: `Banshee Babe`,
+                start_url: `/`,
+                background_color: `#6b37bf`,
+                theme_color: `#6b37bf`,
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: `standalone`,
+                icon: `src/images/icon.png`, // This path is relative to the root of the site.
+            }
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+    ],
+}
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,57 @@
+export default {
+    colors: {
+        text: "rgba(112, 205, 255, 1)",
+        background: "rgba(66, 0, 57, 1)",
+        primary: "rgba(139, 249, 124, 1)",
+        secondary: "rgba(240, 140, 174, 1)",
+    },
+    fonts: {
+        body: "Imprima, system-ui, sans-serif",
+        heading: "Arima Madurai, system-ui, sans-serif",
+        subheading: "Bad Script, cursive",
+        monospace: "Menlo, monospace",
+    },
+    fontWeights: {
+        body: 400,
+        subheading: 550,
+        heading: 700,
+        bold: 700,
+    },
+    lineHeights: {
+        body: 1.5,
+        heading: 1.125,
+    },
+    fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+    space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+    styles: {
+        // the keys used here reference elements in MDX
+        h1: {
+            // the style object for each element
+            // can reference other values in the theme
+            fontFamily: "heading",
+            fontWeight: "heading",
+            lineHeight: "heading",
+            marginTop: 0,
+            marginBottom: 3,
+        },
+        h2: {
+            fontFamily: "subheading",
+            fontWeight: "subheading",
+            lineHeight: "body",
+            marginTop: 1,
+            marginBottom: 1,
+        },
+        a: {
+            fontFamily: "subheading",
+            color: "primary",
+            ":hover, :focus": {
+                color: "secondary",
+            },
+        },
+        p:{
+            color: "secondary",
+            backgroundColor: 'primary',
+        }
+        // more styles can be added as needed
+    },
+}
