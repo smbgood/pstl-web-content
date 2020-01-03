fb03c72e1c00a3b6783c1f125938858160859905
Chase G Turner <chasegregoryturner@gmail.com>
2020-01-03 10:05:38 -0500

    Sidebar for full width display added

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import {graphql, StaticQuery} from 'gatsby'
 import "../styles/blog.scss"
 import Img from "gatsby-image"
@@ -7,6 +7,11 @@ class Blog extends Component {
   // Initialise Stripe.js with your publishable key.
   // You can find your key in the Dashboard:
   // https://dashboard.stripe.com/account/apikeys
+
+  constructor(props) {
+    super(props);
+    //this.handleScroll = this.handleScroll.bind(this)
+  }
 
   createFilterArrayForBlogImages(props) {
     let retString = "";
@@ -58,10 +63,77 @@ class Blog extends Component {
     return fluidReturn
   }
 
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    //let scrollTop = event.srcElement.body.scrollTop;
+    //console.log(scrollTop)
+    //console.log(event.target.body.scrollTop)
+
+    let body = this.document.body,
+      html = this.document.documentElement;
+
+    let height = Math.max( body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    let valToBreak = window.pageYOffset + window.innerHeight;
+
+    let valToExceed = height - 53.3;
+
+    console.log(valToBreak + "  " + valToExceed)
+
+    if (window.pageYOffset > 64 && valToBreak < valToExceed) {
+      let item = event.target.querySelector(".blog-hover-bar")
+      if(item) {
+        item.className = "blog-hover-bar-sticky"
+      }else{
+        let item = event.target.querySelector(".blog-hover-bar-sticky-btm")
+        if(item) {
+          item.className = "blog-hover-bar-sticky"
+        }
+      }
+    }else if (valToBreak > valToExceed) {
+      let item = event.target.querySelector(".blog-hover-bar")
+      if(item) {
+        item.className = "blog-hover-bar-sticky-btm"
+      }else{
+        let item = event.target.querySelector(".blog-hover-bar-sticky")
+        if(item) {
+          item.className = "blog-hover-bar-sticky-btm"
+        }
+      }
+    }else{
+      let item = event.target.querySelector(".blog-hover-bar-sticky")
+      if(item) {
+        item.className = "blog-hover-bar"
+      }else{
+        let item = event.target.querySelector(".blog-hover-bar-sticky-btm")
+        if(item) {
+          item.className = "blog-hover-bar"
+        }
+      }
+    }
+
+    /*let scrollTop = event.srcElement.body.scrollTop,
+      itemTranslate = Math.min(0, scrollTop/3 - 60);
+
+    this.setState({
+
+    this.setState({
+      transform: itemTranslate
+    });*/
+    //console.log(event)
+  }
+
   render() {
-    console.log(this.props);
-    const filterArray = this.createFilterArrayForBlogImages(this.props)
-    console.log(filterArray)
+
+/*    const [listenerAdded, setListenerAdded] = useState(false);*/
 
     return (
 
@@ -72,12 +144,18 @@ class Blog extends Component {
                   <div className="blog-image-container">
                   {this.findImageForSetBlogImage(this.props, edge) != null ? (<Img fluid={this.findImageForSetBlogImage(this.props, edge)} />) : console.log("no")}
                   </div>
-                  <h1>{edge.node.frontmatter.title}</h1>
-                  <p>{edge.node.excerpt}</p>
-                  <a className="blog-page-link btn btn-small waves-purple" href={"/" + edge.node.frontmatter.path}>Go to Blog</a>
+                  <div className="blog-detail-container">
+                    <h1>{edge.node.frontmatter.title}</h1>
+                    <p>{edge.node.excerpt}</p>
+                    <a className="blog-page-link btn btn-small waves-purple" href={"/" + edge.node.frontmatter.path}>Go to Blog</a>
+                  </div>
                 </div>
               </div>
             ))}
+            {/*this.props.scroll != null && this.props.scroll > 64 ? "sticky" : ""*/}
+            <div className="blog-hover-bar">
+              whooo
+            </div>
           </div>
         )
   }
