caed5d3bf020a1a005aad20870f1b5835fab0603
Chase Turner <chasegregoryturner@gmail.com>
2020-02-22 12:12:59 -0500

    Removing starter blogs

--- src/pages/shope.js
+++ src/pages/shope.js
@@ -1,4 +1,4 @@
-import {React, Component} from "react"
+import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/page/layout"
 import Collage from "../components/old/collage"
@@ -11,36 +11,20 @@ import About from "../components/page/about"
 import Contact from "../components/page/contact"
 import { graphql, useStaticQuery } from "gatsby"
 
-import {createBrowserHistory} from "history";
-
-class Shope extends Component{
-
-    componentDidMount(){
-        /*let history = createBrowserHistory()
-
-        history.listen((location, action) => {
-            console.log(`The current URL is ${location.pathname}${location.search}${location.hash}`)
-            console.log(`The last navigation action was ${action}`)
-        })*/
-    }
-
-    render({data}){
-        return( <Layout navImage={data.logoImage.edges[0].node}>
-            <SEO title={"Banshee Babe Boutique | Trinkets, Odds & Ends"} />
-            <Router>
-                <Collage path="/shope/collage" />
-                <Shop path="/shope/shop" />
-                <Categories path="/shope/categories"/>
-                <Blog path="/shope" blogs={data}/>
-                <Home path="/shope/home"/>
-                <About path="/shope/about"/>
-                <Contact path="/shope/contact"/>
-            </Router>
-        </Layout>)
-    }
-
-}
-export default Shope
+const Shope = ({data}) => (
+    <Layout navImage={data.logoImage.edges[0].node}>
+        <SEO title={"Banshee Babe Boutique | Trinkets, Odds & Ends"} />
+        <Router>
+            <Collage path="/shope/collage" />
+            <Shop path="/shope/shop" />
+            <Categories path="/shope/categories"/>
+            <Blog path="/shope" blogs={data}/>
+            <Home path="/shope/home"/>
+            <About path="/shope/about"/>
+            <Contact path="/shope/contact"/>
+        </Router>
+    </Layout>
+)
 export const query = graphql`
     query ShopeQuery {
         blogResults: allMdx(sort: {fields: frontmatter___date}) {
@@ -80,3 +64,4 @@ export const query = graphql`
         }
     }
 `
+export default Shope
