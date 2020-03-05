e6f99e828ccdc6d7c47f12e3d251c95bc003bd42
Chase Turner <chasegregoryturner@gmail.com>
2020-03-05 05:21:41 -0500

    Changing to simplest form

--- src/components/page/contact.js
+++ src/components/page/contact.js
@@ -11,60 +11,20 @@ class Contact extends Component {
     return (
       <div className="contact-root">
         <h1 className="contact-title">Contact Us</h1>
-          <Formik
-              initialValues={{ name: '', email: '', message: '' }}
-              validate={values => {
-                  const errors = {};
-                  if (!values.email) {
-                      errors.email = 'Email required';
-                  }else if (
-                      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                  ) {
-                      errors.email = 'Invalid email address';
-                  }
+          <form name="b-b-contact" data-netlify="true" netlify-honeypot="bot-field" method="POST" action="/" netlify>
+              <input type="hidden" name="bot-field"/>
+              <input type="hidden" name="form-name" value="b-b-contact" />
+              <input type="text" name="name" placeholder="What's your name?"/>
+              <br/>
+              <input type="email" name="email" placeholder="Enter a good contact email" />
+              <br/>
+              <input name="message" type="textarea" placeholder="Here is where you leave me your glowing messages or ask any questions!" />
+              <br/>
+              <button type="submit" >
+                  Submit
+              </button>
+          </form>
 
-                  if(!values.name){
-                      errors.name = 'Please enter your name';
-                  }
-                  return errors;
-              }}
-              onSubmit={(values, { setSubmitting }) => {
-                  values = qs.stringify(values)
-                  axios.post(this.props.location.pathname, values, {headers: {"Content-Type":"application/x-www-form-urlencoded"}})
-                      .then(response => {
-                          //used to parse out stuff to use on the spot
-                          /*const {
-                              data: {
-                                  userId: id
-                              }
-                          } = profile*/
-
-                          console.log(response)
-                      }).catch(error => {
-                      console.log(error)
-                  })
-                  setSubmitting(false);
-              }}
-          >
-              {({ isSubmitting }) => (
-                  <Form name="b-b-contact" data-netlify="true" netlify-honeypot="bot-field" method="POST" action="/" netlify>
-                      <input type="hidden" name="bot-field"/>
-                      <input type="hidden" name="form-name" value="b-b-contact" />
-                      <Field name="name" placeholder="What's your name?"/>
-                      <ErrorMessage name="name" component="div"/>
-                      <br/>
-                      <Field type="email" name="email" placeholder="Enter a good contact email" />
-                      <ErrorMessage name="email" component="div" />
-                      <br/>
-                      <Field name="message" component="textarea" placeholder="Here is where you leave me your glowing messages or ask any questions!" />
-                      <ErrorMessage name="message" component="div" />
-                      <br/>
-                      <button type="submit" disabled={isSubmitting}>
-                          Submit
-                      </button>
-                  </Form>
-              )}
-          </Formik>
       </div>
     )
   }
