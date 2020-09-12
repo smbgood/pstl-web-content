6b5951c76d8cdbc6185ca83ac7dd6e260624e3f8
Chase Turner <chasegregoryturner@gmail.com>
2020-09-12 03:23:04 -0400

    Changes to go live

--- src/components/page/cart.js
+++ src/components/page/cart.js
@@ -127,7 +127,7 @@ class Cart extends Component {
                                             addressValues["validate"] = true
                                             let responseData = null
                                             let orderId = ""
-                                            let url = "http://localhost:9090/banshee/addr?" + qs.stringify(addressValues)
+                                            let url = "http://richornot.com/banshee/addr?" + qs.stringify(addressValues)
                                             await axios.get(url).then(response => {
                                                 //if valid, show them how much to ship,
                                                 if (response && response.data) {
