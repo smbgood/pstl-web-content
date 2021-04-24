cc3b7f4c68c5b2b8ac8a4c5d7ee87404c6e9094d
Chase Turner <chasegregoryturner@gmail.com>
2021-04-24 15:46:37 -0400

    Updated some stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo";
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import "../styles/welcome.scss"
 import "../styles/contact.scss"
 import {GatsbyImage} from "gatsby-plugin-image";
@@ -26,25 +26,31 @@ function param(object) {
   return encodedString;
 }
 
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 const Index = ({data, location}) => (
     <Layout navImage={data.logoImage.edges[0].node} location={location}>
         <SEO title={"Dave's Truck Barrels"} history={location}/>
         <div className={"welcome-root"}>
           <div className={"welcome-section"}>
             <div className={"welcome-image"}>
-              <GatsbyImage image={data.comingSoonImage.childImageSharp.gatsbyImageData} alt={"Coming Soon"}/>
+              <GatsbyImage image={data.newLogo.childImageSharp.gatsbyImageData} alt={"Coming Soon"}/>
               <div className={"welcome-overlay"}>
                 <div className={"welcome-top"}>
-                  Welcome to davesbarrels.com! Please mind the mess as we are undergoing construction.
+                  Welcome to davesbarrels.com! <br/> Please mind the mess as we are undergoing construction.
                 </div>
                 <div className={"welcome-bottom"}>
-                  however, you can still get in contact with us below while we are building!
+                  <br/>
+                  In the mean time, you can get in contact with us below while we are building!
                 </div>
               </div>
             </div>
           </div>
           <div className="contact-root">
-            <h1 className="contact-title">Contact Us</h1>
             <Formik
               initialValues={{ firstname: '', email: '', message: '' }}
               validate={values => {
@@ -63,8 +69,18 @@ const Index = ({data, location}) => (
                 return errors;
               }}
               onSubmit={(values, { setSubmitting }) => {
-                values["form-name"] = "pstl"
-                values = qs.stringify(values)
+                fetch('/', {
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                  body: encode({
+                    'form-name': 'pstl',
+                    ...values,
+                  }),
+                })
+                  .then(() => alert("success"))
+                  .catch((error) => alert(error))
+
+                //values = qs.stringify(values)
                 /*var url = "/pstl-contact";
                 const options = {
                   method: 'POST',
@@ -88,15 +104,15 @@ const Index = ({data, location}) => (
                   console.log(error)
                   setSubmitting(false);
                 })*/
-                values = qs.stringify(values)
+                /*values = qs.stringify(values)
                 axios.post("/", values, {headers: {'Content-Type':'application/x-www-form-urlencoded'}})
                   .then(response => {
                     //used to parse out stuff to use on the spot
-                    /*const {
+                    /!*const {
                         data: {
                             userId: id
                         }
-                    } = profile*/
+                    } = profile*!/
 
                     console.log(response)
                     if (typeof window !== `undefined`) window.location.replace(`/success`)
@@ -104,15 +120,19 @@ const Index = ({data, location}) => (
                   }).catch(error => {
                   console.log(error)
                   setSubmitting(false);
-                })
+                })*/
               }}
             >
               {({ isSubmitting }) => (
-                <Form name="pstl" data-netlify="true" netlify-honeypot="bot-field" method="post" action="/success" >
-                  <input type="hidden" name="bot-field"/>
-                  <input type="hidden" name="form-name" value="pstl"/>
-                  <br/>
-                  <br/>
+                <Form name="pstl" method="post" action="/success" data-netlify="true"
+                      data-netlify-honeypot="bot-field" >
+                  <input type="hidden" name="form-name" value="pstl" />
+                  <div hidden>
+                    <label>
+                      Don’t fill this out:{' '}
+                      <input name="bot-field" />
+                    </label>
+                  </div>
                   <Field name="firstname" placeholder="What's your name?"/>
                   <ErrorMessage name="firstname" component="div"/>
                   <br/>
@@ -129,6 +149,9 @@ const Index = ({data, location}) => (
               )}
             </Formik>
           </div>
+          <div className={"coming-soon-section"}>
+            <GatsbyImage image={data.comingSoonImage.childImageSharp.gatsbyImageData} alt={"Coming Soon"}/>
+          </div>
         </div>
     </Layout>
 )
@@ -144,6 +167,11 @@ export const query = graphql`
                 }
             }
         },
+        newLogo: file(relativePath: { eq :"pstl-logo.png"}) {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED)
+            }
+        }
         welcomeInfo: allSiteadminJson {
             nodes {
               welcomepage {
